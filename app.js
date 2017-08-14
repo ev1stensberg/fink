b768754b3fe858d92963e233de309a8e56b6b4a1
Even Stensberg <evenstensberg@gmail.com>
2017-08-14 02:59:16 +0200

    make a new commit before game of thrones

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const {dialog} = require('electron').remote
 const {readFileSync, readdirSync, existsSync} = require('fs');
 const path = require('path');
+const {spawn} = require('child_process');
 
 document.getElementById("main-bttn").addEventListener("click", function(e) {
     dialog.showOpenDialog({properties: [ 'openDirectory']}, function(dir) {
@@ -15,7 +16,12 @@ document.getElementById("main-bttn").addEventListener("click", function(e) {
             const pkgJSON = require(pkgJSONPath);
             const appendToElement = document.querySelector('section');
             const headerMenu = document.createElement('div');
+
+            const mainArea = document.createElement('div');
+            mainArea.className = "main-terminal";
+            appendToElement.appendChild(mainArea);
             appendToElement.appendChild(headerMenu);
+
             headerMenu.className = "header-menu";
 
             Object.keys(pkgJSON.scripts).forEach( (script) => {
@@ -26,8 +32,8 @@ document.getElementById("main-bttn").addEventListener("click", function(e) {
                 scriptSection.addEventListener("mouseover", function(e) {
                     
                     if(!toolTipBox) {
-                    toolTipBox = document.createElement('div');
-                    toolTipBox.textContent = pkgJSON.scripts[script];
+                    toolTipBox = document.createElement('span');
+                    toolTipBox.innerHTML = pkgJSON.scripts[script];
                     toolTipBox.className = "tooltip-box";
                     scriptSection.appendChild(toolTipBox)
                     } else {
@@ -37,6 +43,24 @@ document.getElementById("main-bttn").addEventListener("click", function(e) {
                  scriptSection.addEventListener("mouseleave", function(e) {
                     e.target.removeChild(e.target.children[0]);
                 })
+                scriptSection.addEventListener("click", function(e) {
+                    const cmd = pkgJSON.scripts[script].split(' ');
+                    const firstArg = cmd.shift();
+                    const child = spawn(firstArg, cmd, {
+                        cwd: path.resolve(dir[0])
+                    })
+                    child.stdout.on('data', (data) => {
+                        console.log(`stdout: ${data}`);
+                    });
+                    
+                    child.stderr.on('data', (data) => {
+                        console.log(`stderr: ${data}`);
+                    });
+                    child.on('close', (code) => {
+                        console.log(`child process exited with code ${code}`);
+                    });
+                })
+
                 headerMenu.appendChild(scriptSection);
             })
 
